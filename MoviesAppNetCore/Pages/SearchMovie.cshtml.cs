751d05ee0f25a4563bac8c1a32c2f0c8f245b08b
mounika.rendedla <mounika.rendedla@extendhealth.com>
2018-06-26 21:05:20 -0600

    Razor pages

---
+++ MoviesAppNetCore/Pages/SearchMovie.cshtml.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MoviesAppNetCore.Models;
+using MoviesAppNetCore.Services;
+
+namespace MoviesAppNetCore.Pages
+{
+    public class SearchMovieModel : PageModel
+    {
+        private IMovieRepository _repository;
+
+        public SearchMovieModel(IMovieRepository movieRepository)
+        {
+            _repository = movieRepository;
+        }
+
+        public Movie movie { get; set; }
+
+        public void OnGet(int id = 1)
+        {
+            movie = _repository.getMovie(id);
+
+        }
+    }
+}
